cdec53c129a7c187f2dfed5d79198d96e7b273e2
vivemeno <viveksmenon87@gmail.com>
2019-10-06 19:21:59 -0700

    Commiting CMAke Files

---
+++ cmake-build-debug/CMakeFiles/ThreadsImpl2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ThreadsImpl2.dir/main.c.o"
+  "ThreadsImpl2"
+  "ThreadsImpl2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/ThreadsImpl2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
